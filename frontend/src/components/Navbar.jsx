e5d809f7c63bcb9eecc5038df60d1d129a7e51d5
Sreya Anil <sreyaanilcbe@gmail.com>
2026-07-24 23:17:54 +0530

    Add basic navigation bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,15 @@ function Navbar() {
   return (
     <nav>
       <h2>Matchly</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <button>Get Started</button>
     </nav>
   );
 }
